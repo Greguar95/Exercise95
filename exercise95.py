4e740d4de778404f48c7d1d22c1f739b155cd91c
Greg <qosound@yahoo.com>
2023-10-22 15:14:25 -0500

    Ex951

---
+++ exercise95.py
@@ -0,0 +1,16 @@
+fname = input('Enter file:')
+try:
+    fhand = open(fname)
+except:
+    fname = "mbox-short.txt"
+    fhand = open(fname)
+dc = dict()
+for line in fhand:
+    if line.startswith('From '):
+        line = line.rstrip()
+        words = line.split()
+        eml = words[1]
+        splt = eml.split('@')
+        hst = splt[1]
+        dc[hst] = dc.get(hst, 0) + 1
+print(dc)
